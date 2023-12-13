20b89d939a29b5456ed54559e6965cd4248a0098
Tiane Zerba <148304153+tianenaomi@users.noreply.github.com>
2023-12-13 13:14:04 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+
+}
+
+console.log();
+
+// PROBLEM
+// User selects 1 of 3 options
+// Computer selects 1 0f 3 options
+// User and Computer selections revealed simultaneously
+// Winner is declared  
+    // OR tie
+    // if tie, run again
+
+
+// PLAN
+// Interface = false. 
